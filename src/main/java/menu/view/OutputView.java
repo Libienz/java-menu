efb164c65a6c90d03d0d0e0f5db5af81631e3ab1
libienz <Leekh10195@gmail.com>
2023-12-12 14:25:56 +0900

    feat: OutputView 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,28 @@
+package menu.view;
+
+import java.util.List;
+import menu.domain.Coach;
+import menu.domain.FoodCategory;
+
+public class OutputView {
+    private final OutputMessageResolver outputMessageResolver;
+
+    public OutputView(OutputMessageResolver outputMessageResolver) {
+        this.outputMessageResolver = outputMessageResolver;
+    }
+
+    public void printGreeting() {
+        System.out.println(outputMessageResolver.resolveGreetingMessage());
+    }
+
+    public void printDone() {
+        System.out.println(outputMessageResolver.resolveDoneMessage());
+    }
+
+    public void printResult(List<Coach> coaches, List<FoodCategory> categories) {
+        System.out.println(outputMessageResolver.resolveDayOfWeekMessage());
+        System.out.println(outputMessageResolver.resolveSelectedCategoryMessage(categories));
+        coaches.forEach(coach -> System.out.println(outputMessageResolver.resolveCoachFoodsMessage(coach)));
+    }
+
+}
