956b821abc936e7d40fe28d60ca848fc9650e0b9
libienz <Leekh10195@gmail.com>
2023-12-12 21:16:08 +0900

    feat: 메뉴 카테고리 enum에 이름 속성 추가

--- src/main/java/menu/domain/MenuCategory.java
+++ src/main/java/menu/domain/MenuCategory.java
@@ -3,17 +3,19 @@ package menu.domain;
 import java.util.Arrays;
 
 public enum MenuCategory {
-    JAPANESE(1),
-    KOREAN(2),
-    CHINESE(3),
-    ASIAN(4),
-    WESTERN(5);
+    JAPANESE(1, "일식"),
+    KOREAN(2, "한식"),
+    CHINESE(3, "중식"),
+    ASIAN(4, "아시안"),
+    WESTERN(5, "양식");
 
     private static final String NO_SUCH_CATEGORY_MESSAGE = "[ERROR] 해당하는 카테고리가 존재하지 않습니다";
     private final int code;
+    private final String name;
 
-    MenuCategory(int code) {
+    MenuCategory(int code, String name) {
         this.code = code;
+        this.name = name;
     }
 
     public static MenuCategory from(int code) {
@@ -21,5 +23,13 @@ public enum MenuCategory {
                 .filter(menuCategory -> menuCategory.code == code)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_CATEGORY_MESSAGE));
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
     }
 }
