51e5af87f7defa9f700022bb7fe37c785146af12
libienz <Leekh10195@gmail.com>
2023-12-12 14:05:58 +0900

    feat: 음식 추천 메서드 조건을 고려하도록 수정

--- src/main/java/menu/service/FoodMenuSuggestionService.java
+++ src/main/java/menu/service/FoodMenuSuggestionService.java
@@ -1,14 +1,18 @@
 package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import menu.domain.Coach;
 import menu.domain.FoodCategory;
 import menu.domain.FoodMenu;
 
 public class FoodMenuSuggestionService {
     private static final int SUGGEST_INDEX = 0;
 
-    public FoodMenu suggest(FoodCategory category) {
+    public void suggest(FoodCategory category, Coach coach) {
         String foodName = Randoms.shuffle(FoodMenu.findFoodsByCategory(category)).get(SUGGEST_INDEX);
-        return FoodMenu.from(foodName);
+        FoodMenu suggestedFood = FoodMenu.from(foodName);
+        while (!coach.addSuggestion(suggestedFood)) {
+            continue;
+        }
     }
 }
