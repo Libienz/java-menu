5cf6376fbda12f73a688dcf5c272da8ce63d247a
libienz <Leekh10195@gmail.com>
2023-12-12 14:04:56 +0900

    feat: 코치 클래스에 추천받은 음식들 관리하도록 속성 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,8 +1,14 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
+    private static final int MAX_CATEGORY_COUNT = 2;
+
     private final CoachName name;
     private final InedibleFoods inedibleFoods;
+    private List<FoodMenu> suggestedFoods = new ArrayList<>();
 
     public Coach(CoachName name, InedibleFoods inedibleFoods) {
         this.name = name;
@@ -15,5 +21,15 @@ public class Coach {
 
     public boolean isEdible(FoodMenu foodMenu) {
         return !inedibleFoods.contains(foodMenu);
+    }
+
+    public boolean addSuggestion(FoodMenu suggestion) {
+        if (!isEdible(suggestion)) {
+            return false;
+        }
+        if (suggestedFoods.contains(suggestion)) {
+            return false;
+        }
+        return suggestedFoods.add(suggestion);
     }
 }
