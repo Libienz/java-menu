faa1709643fd32dcc63fe1cf454aa5837ee74149
libienz <Leekh10195@gmail.com>
2023-12-12 19:59:13 +0900

    feat: 식단 일정표 클래스 작성

---
+++ src/main/java/menu/domain/FoodSchedule.java
@@ -0,0 +1,17 @@
+package menu.domain;
+
+import java.time.DayOfWeek;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FoodSchedule {
+    private final Map<DayOfWeek, FoodMenu> schedule = new HashMap<>();
+
+    public void addSchedule(DayOfWeek dayOfWeek, FoodMenu foodMenu) {
+        schedule.put(dayOfWeek, foodMenu);
+    }
+
+    public boolean contains(FoodMenu foodMenu) {
+        return schedule.containsValue(foodMenu);
+    }
+}
