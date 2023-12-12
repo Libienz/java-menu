1b11e0462bc415e24a8f5f3f4f24dd86c5da345e
libienz <Leekh10195@gmail.com>
2023-12-12 21:12:12 +0900

    refactor: 생성 로직 정적 팩토리 메서드로 통일

--- src/main/java/menu/dto/FoodScheduleDto.java
+++ src/main/java/menu/dto/FoodScheduleDto.java
@@ -7,7 +7,7 @@ public class FoodScheduleDto {
     private final String coachName;
     private final List<String> foodSchedule;
 
-    public FoodScheduleDto(String coachName, List<String> foodSchedule) {
+    private FoodScheduleDto(String coachName, List<String> foodSchedule) {
         this.coachName = coachName;
         this.foodSchedule = foodSchedule;
     }
