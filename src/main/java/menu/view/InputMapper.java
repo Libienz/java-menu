da9876438e96c3a0f870bf2e386781c24414b91e
libienz <Leekh10195@gmail.com>
2023-12-12 19:30:24 +0900

    feat: InputMapper 먹을 수 없는 음식 클래스로 매핑하는 기능 추가

--- src/main/java/menu/view/InputMapper.java
+++ src/main/java/menu/view/InputMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import menu.domain.CoachName;
 import menu.domain.CoachNames;
+import menu.domain.FoodMenu;
+import menu.domain.InedibleFoods;
 
 public class InputMapper {
     private static final String DELIMITER = ",";
@@ -14,5 +16,12 @@ public class InputMapper {
                 .map(CoachName::new)
                 .collect(Collectors.toList());
         return new CoachNames(names);
+    }
+
+    public InedibleFoods mapToInedible(String input) {
+        List<FoodMenu> inedible = Arrays.stream(input.split(DELIMITER))
+                .map(FoodMenu::from)
+                .collect(Collectors.toList());
+        return new InedibleFoods(inedible);
     }
 }
