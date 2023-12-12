5e9bbd90bbdc7c58ea57218294495d58dd116f61
libienz <Leekh10195@gmail.com>
2023-12-12 20:27:47 +0900

    feat: 결과 출력 메시지 생성 기능 추가

--- src/main/java/menu/view/MessageResolver.java
+++ src/main/java/menu/view/MessageResolver.java
@@ -1,7 +1,18 @@
 package menu.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.dto.FoodScheduleDto;
+import menu.dto.MenuCategoriesDto;
+
 public class MessageResolver {
     private static final String INPUT_COACH_NAMES_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String GREETING_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private static final String RESULT_MESSAGE_PREFIX = "메뉴 추천 결과입니다.";
+    private static final String DONE_MESSAGE = "추천을 완료했습니다.";
+    private static final String VALUES_MESSAGE_PREFIX = "[ ";
+    private static final String VALUES_MESSAGE_DELIMITER = " | ";
+    private static final String VALUES_MESSAGE_POSTFIX = " ]";
     private static final String INPUT_COACH_INEDIBLE_MESSAGE_FORMAT = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public String resolveInputCoachNamesMessage() {
@@ -10,5 +21,24 @@ public class MessageResolver {
 
     public String resolveInputInedibleMessage(String coachName) {
         return String.format(INPUT_COACH_INEDIBLE_MESSAGE_FORMAT, coachName);
+    }
+
+    public String resolveCategoryMessage(MenuCategoriesDto menuCategoriesDto) {
+        return formatValuesMessage(menuCategoriesDto.getMenuCategories());
+    }
+
+    public String resolveFoodScheduleMessage(FoodScheduleDto foodScheduleDto) {
+        List<String> foodSchedule = foodScheduleDto.getFoodSchedule();
+        foodSchedule.add(0, foodScheduleDto.getCoachName());
+        return formatValuesMessage(foodSchedule);
+    }
+
+    public String resolveDoneMessage() {
+        return DONE_MESSAGE;
+    }
+
+    public String formatValuesMessage(List<String> values) {
+        return values.stream()
+                .collect(Collectors.joining(VALUES_MESSAGE_DELIMITER, VALUES_MESSAGE_PREFIX, VALUES_MESSAGE_POSTFIX));
     }
 }
