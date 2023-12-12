6eb3d2789c7bd4aba47a8c4829a66c75587910af
libienz <Leekh10195@gmail.com>
2023-12-12 11:34:44 +0900

    feat: InputMapper 구현

---
+++ src/main/java/menu/view/InputMapper.java
@@ -0,0 +1,30 @@
+package menu.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.domain.CoachName;
+import menu.domain.CoachNames;
+import menu.domain.FoodMenu;
+import menu.domain.InedibleFoods;
+
+public class InputMapper {
+    private static final String SEPARATOR = ",";
+    public CoachNames mapToCoachNames(String input) {
+        return new CoachNames(splitBySeparator(input)
+                .stream()
+                .map(CoachName::new)
+                .collect(Collectors.toList()));
+    }
+
+    public InedibleFoods mapToInedibleFood(String input) {
+        return new InedibleFoods(splitBySeparator(input)
+                .stream()
+                .map(FoodMenu::from)
+                .collect(Collectors.toList()));
+    }
+
+    private List<String> splitBySeparator(String input) {
+        return Arrays.asList(input.split(SEPARATOR));
+    }
+}
