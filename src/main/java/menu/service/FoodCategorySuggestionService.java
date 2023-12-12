4c016c79a3ad48294f201078dbfd3f9a67d75014
libienz <Leekh10195@gmail.com>
2023-12-12 14:05:33 +0900

    feat: 카테고리 추가 시 조건을 판단하며 추가하도록 기능 개선

--- src/main/java/menu/service/FoodCategorySuggestionService.java
+++ src/main/java/menu/service/FoodCategorySuggestionService.java
@@ -1,13 +1,28 @@
 package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 import menu.domain.FoodCategory;
+import menu.domain.FoodMenu;
 
 public class FoodCategorySuggestionService {
+    private static final int MAX_CATEGORY_COUNT = 2;
     private static final int MIN_CATEGORY_CODE = 1;
     private static final int MAX_CATEGORY_CODE = 5;
 
-    public FoodCategory suggest() {
-        return FoodCategory.from(Randoms.pickNumberInRange(MIN_CATEGORY_CODE, MAX_CATEGORY_CODE));
+    public FoodCategory suggest(List<FoodCategory> suggestedCategory) {
+        while(true) {
+            FoodCategory category = FoodCategory.from(Randoms.pickNumberInRange(MIN_CATEGORY_CODE, MAX_CATEGORY_CODE));
+            int count = countCategorySuggested(suggestedCategory, category);
+            if (count + 1 < MAX_CATEGORY_COUNT) {
+                return category;
+            }
+        }
+    }
+
+    public int countCategorySuggested(List<FoodCategory> foodCategories, FoodCategory target) {
+        return (int) foodCategories.stream()
+                .filter(target::equals)
+                .count();
     }
 }
